16616ea86f23c0341a437ec71e9387f79fe1030a
mpraiser <mraiser@foxmail.com>
2020-11-19 14:08:42 +0800

    sample main

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from bin_merger import BinaryFile, Merger
 
 x1 = BinaryFile(path='bootloader.bin')
-x2 = BinaryFile(path='funflash.bin')
-x3 = BinaryFile(path='entryinit.bin')
+x2 = BinaryFile(path='image1.bin')
+x3 = BinaryFile(path='image2.bin')
 
 y = Merger(
     [x1, x2, x3],
-    [0x00000000, 0x0003D800, 0x0003E000]
+    [0x00000000, 0x00010000, 0x00020000]
     ).merge()
 y.save('target.bin')
